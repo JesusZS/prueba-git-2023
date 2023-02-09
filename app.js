2d399a97a40734508410a807ce844ce749c8a352
Gsus <js49831@gmail.com>
2023-02-09 09:16:38 -0500

    se agrega 'app.js' con funcion 'restar()'

---
+++ app.js
@@ -0,0 +1,18 @@
+let n1 = 7;
+let n2 = 3;
+
+
+/**
+ * 
+ * @param {*} num1 
+ * @param {*} num2 
+ * @returns 
+ */
+function restar(num1, num2) {
+    let resultado = num1 - num2;
+    console.log(`el resultado de la resta es: ${resultado}`);
+
+    return resultado;
+}
+
+restar(n1, n2);
